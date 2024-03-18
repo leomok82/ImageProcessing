28f03b7d50be1425d6dd9c9af519405e9da2200d
Your Name <youremail@domain.com>
2024-03-18 15:45:59 +0000

    imageBlur

---
+++ src/ImageBlur/ImageBlur/ImageBlur/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ImageBlur
+//
+//  Created by Ke, Zeyi on 18/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

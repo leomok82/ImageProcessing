87986120e6c2bbf909c1cbcc04b1b766cb8e960c
Bofan <143876229+edsml-bl1023@users.noreply.github.com>
2024-03-20 19:58:54 +0000

    Modify tests

--- test/main.cpp
+++ test/main.cpp
@@ -7,11 +7,11 @@
 
 // "g++ -o test test.cpp ../src/new/ColourFilterCorrection.cpp ../src/new/ColorConversion.cpp main.cpp -std=c++17" to use this test.
 int main() {
-    testGrayscaleFilter();
-    testBrightnessFilter();
+    // testGrayscaleFilter();
+    // testBrightnessFilter();
     testHistogramEqualizerFilter();
-    testThresholdingFilter();
-    testSaltAndPepperNoiseFilter();
+    // testThresholdingFilter();
+    // testSaltAndPepperNoiseFilter();
 
     // testLoadApplyAndSaveImage();
     return 0;
